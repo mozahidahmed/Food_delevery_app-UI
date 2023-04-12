a78c972fc312276be1492e0fda58a5ef48948b48
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-04-12 19:20:19 +0600

    2xl

--- src/components/pages/Hero1.js
+++ src/components/pages/Hero1.js
@@ -4,7 +4,7 @@ const Hero1 = () => {
     return (
         <div className=''>
             <hr className='mx-24 mb-12' />
-            <h1 className='lg:text-3xl font-bold text-center '>How the app works</h1>
+            <h1 className='text-3xl font-bold text-center '>How the app works</h1>
             <div className="grid sm:grid-cols-1 lg:grid-cols-2 px-24 mt-24 justify-center items-center">
 
                 <div className="grid justify-center shadow-[50px 30px 150px 0px #2E040426]">
@@ -14,7 +14,7 @@ const Hero1 = () => {
                 <div className="grid justify-center   md:order-last  order-first ">
 
                     <h3 className='text-[#FA4A0C]  font-bold '>Create an account</h3>
-                    <h2 className='font-bold text-3xl mt-2 mb-2'>Create/login to an existing <br />
+                    <h2 className='font-bold sm:text-2xl lg:text-3xl mt-2 mb-2'>Create/login to an existing <br />
                         account to get started</h2>
                     <h4>An account is created with your email <br />
                         An account is created with your email
